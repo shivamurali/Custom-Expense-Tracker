adce5d56d04fe4969853e8d6a57ae50c2ed6e55c
shivamurali <pandiham@gmail.com>
2023-01-19 16:42:22 -0500

    First Push

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h2>Anita is sooo stinky!</h2>
+      <p> Sample paragraph test should also be visible</p>
+    </div>
+  );
+}
+
+export default App;
